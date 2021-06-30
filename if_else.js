b6bba4955dffc046c36819c1c5c4d544ca6f17da
Aadesh Agrawal <74392722+aadesh-2806@users.noreply.github.com>
2021-06-30 22:54:35 +0530

    Add files via upload

---
+++ if_else.js
@@ -0,0 +1,54 @@
+console.log("Hello Welcome To The Program");
+let va;
+va = 3+4;
+if(va>9) {
+    console.log("Yes variable is >9")
+}
+else{
+    console.log("No variable is <9")
+}
+const day = prompt("Enter day").toLowerCase();
+if(day==="monday")
+{
+    console.log("monday")
+}
+else{
+    console.log("meh")
+}
+if(day){
+    console.log("TRUTHY")
+}
+else {
+    console.log("FALSY")        //0 undefined   ""  none    ----->false
+}
+if(1!=1 && 2==2 || 3==3)
+{
+    console.log("Yes both sides are true")
+}
+if(1!=1 || 2!=3)
+{
+    console.log("Any one side is true")
+}
+if(!0)
+{
+    console.log("0")
+}
+/*
+&&  >  ||
+*/
+console.log("Thankyou ByeBye");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
